ac9fb83e547475a27d06ef8971d5614f2834d272
Ben Force <xdd741852963@gmail.com>
2022-07-06 15:21:22 +0800

    file and folder structor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import { Actors, MovieInformation, Movies, NavBar, Profile } from '.';
+import useStyles from './styles';
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div>App</div>
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar}>
+          <Routes>
+            <Route exact path="/" element={<Movies />} />
+            <Route exact path="/movies/:id" element={<MovieInformation />} />
+            <Route exact path="/actors/:id" element={<Actors />} />
+            <Route exact path="/profile/:id" element={<Profile />} />
+          </Routes>
+        </div>
+      </main>
+    </div>
   );
 }
 
